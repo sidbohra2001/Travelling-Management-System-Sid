75130c780ed3b92118168b274f034eb060e29ce0
sidbohra <sidmessiah10@gmail.com>
2023-10-13 11:56:29 +0530

    frontend updates

--- frontend/app/src/main/java/com/travel/app/MainActivity.java
+++ frontend/app/src/main/java/com/travel/app/MainActivity.java
@@ -4,25 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.travel.app.layouts.AdminLogin;
 import com.travel.app.layouts.UserLogin;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button userLoginBtn, adminLoginBtn;
+    private Button userBtn, adminBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        userLoginBtn = findViewById(R.id.userLoginBtn);
-        adminLoginBtn = findViewById(R.id.adminLoginBtn);
-        userLoginBtn.setOnClickListener(new View.OnClickListener() {
+        userBtn = findViewById(R.id.userBtn);
+        adminBtn = findViewById(R.id.adminBtn);
+        userBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), UserLogin.class);
+                Toast.makeText(MainActivity.this, "Welcome to User Login "+5, Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
+        adminBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AdminLogin.class);
+                Toast.makeText(MainActivity.this, "Welcome to Admin Login", Toast.LENGTH_SHORT).show();
                 startActivity(intent);
             }
         });
---
+++ frontend/app/src/main/java/com/travel/app/layouts/AdminLogin.java
@@ -0,0 +1,16 @@
+package com.travel.app.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.travel.app.R;
+
+public class AdminLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login_page);
+    }
+}
