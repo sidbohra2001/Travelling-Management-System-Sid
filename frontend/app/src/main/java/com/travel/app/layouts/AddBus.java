87f4c2903a013caafbe2cae564863b42db08ea65
sidbohra <sidmessiah10@gmail.com>
2023-10-24 11:02:29 +0530

    frontend updates

---
+++ frontend/app/src/main/java/com/travel/app/layouts/AddBus.java
@@ -0,0 +1,16 @@
+package com.travel.app.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.travel.app.R;
+
+public class AddBus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_bus_page);
+    }
+}
--- frontend/app/src/main/java/com/travel/app/layouts/AdminBusManagement.java
+++ frontend/app/src/main/java/com/travel/app/layouts/AdminBusManagement.java
@@ -1,16 +1,43 @@
 package com.travel.app.layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.travel.app.R;
 
+import java.util.HashMap;
+
 public class AdminBusManagement extends AppCompatActivity {
+
+    private Button addBusBtn, updateBusBtn, getBusBtn, getBusesBtn, deleteBusBtn;
+    private HashMap<String, String> session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_bus_management_page);
+
+        //Initiating Resources
+        addBusBtn = findViewById(R.id.addBusBtn);
+        updateBusBtn = findViewById(R.id.updateBusBtn);
+        getBusBtn = findViewById(R.id.getBusBtn);
+        getBusesBtn = findViewById(R.id.getBusesBtn);
+        deleteBusBtn = findViewById(R.id.deleteBusBtn);
+        session = (HashMap<String, String>) getIntent().getSerializableExtra("session");
+
+        //Setting onClickListener
+        addBusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminBusManagement.this, AddBus.class);
+                intent.putExtra("session", session);
+                startActivity(intent);
+            }
+        });
     }
 }
